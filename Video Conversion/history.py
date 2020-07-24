dd1308dd8b491dd325230893d8dbba6a7597d8b6
Manish Kumar Singh <61286422+singhmanish1212@users.noreply.github.com>
2020-07-24 15:34:08 +0530

    Add files via upload

---
+++ Video Conversion/history.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Fri Jul 24 14:54:42 2020)---
+runcell(0, 'C:/Users/DELL/.spyder-py3/temp.py')
+runfile('C:/Users/DELL/.spyder-py3/temp.py', wdir='C:/Users/DELL/.spyder-py3')
+pip freeze > requirements.txt
+
+## ---(Fri Jul 24 15:28:00 2020)---
+pip freeze > requirements.txt
