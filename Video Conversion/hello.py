fc015fa77e34042b3fa281018104ea16eb4c6440
Amit Aman <kumaramit_aman@hotmail.com>
2020-07-28 23:50:39 +0530

    * Implemented the method for server.

--- Video Conversion/hello.py
+++ Video Conversion/hello.py
@@ -2,7 +2,7 @@ import sys
 # Takes first name and last name via command 
 # line arguments and then display them 
 print("Output from Python") 
-print("First name: " + sys.argv[1]) 
-print("Last name: " + sys.argv[2]) 
+print("language:" + sys.argv[1]) 
+print("downloadedVideo:" + sys.argv[2]) 
 
 # save the script as hello.py 
